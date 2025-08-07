a1e1b2cb568a165478a014cb8da94dac0e3758d5
DulcesBrii <dulcesbrii2025@gmail.com>
2025-08-07 13:36:09 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,14 @@
+// Tu configuración personalizada de Firebase
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_DOMINIO.firebaseapp.com",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_BUCKET.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
